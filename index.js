8421322d91121e08092464192dff0fcb265ff606
homelyseven250 <61860079+homelyseven250@users.noreply.github.com>
2020-08-03 10:03:11 +0100

    Add command splitting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,12 +7,21 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
+    }
+    else if (command === 'server') {
+        message.channel.send(`Hello, this message is in the channel ${message.channel.name}.
+ The server ${message.guild.name} is in the region ${message.guild.region}`)
     }
     
 });
 
 
-client.login();
+client.login(token);
