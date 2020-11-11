fcc326b7bd123d047e980aecb199f58e8b5c9f4c
homelyseven250 <61860079+homelyseven250@users.noreply.github.com>
2020-11-11 04:56:22 +0000

    no longer voicemod, now Acid's Utilities

---
+++ commands/lockdown.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'lockdown',
+	description: 'Stops user from sending messages.',
+	execute(message, args) {
+        const channel = message.mentions.channels.first();
+        channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false });
+        
+	},
+};
---
+++ commands/unlock.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'unlock',
+	description: 'Lets user from sending messages.',
+	execute(message, args) {
+        const channel = message.mentions.channels.first();
+        channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true });
+        
+	},
+};
