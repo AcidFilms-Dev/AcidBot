200ac6a95e4d3c3e9850e31072cb1e6e9bec6107
homelyseven250 <61860079+homelyseven250@users.noreply.github.com>
2021-01-13 09:32:30 +0000

    update server.js

--- commands/server.js
+++ commands/server.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: "Replies with server info",
 	execute(message, args) {
 		message.channel.send(`Hello, this message is in the channel ${message.channel.name}.
- The server ${message.guild.name} is in the region ${message.guild.region}`)
+ The server ${message.guild.name} is in the region ${message.guild.region}`);
 	},
 };
